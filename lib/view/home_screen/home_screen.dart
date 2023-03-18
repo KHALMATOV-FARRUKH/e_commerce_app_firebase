c614e436ffd002705ef06a84760658d51334b115
KHALMATOV-FARRUKH <khalmatov.farrukh@gmail.com>
2023-03-19 00:01:58 +0500

    modification home_screen.dart
    Created:
    festured_button.dart

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce_app_firebase/consts/consts.dart';
 import 'package:e_commerce_app_firebase/consts/lists.dart';
+import 'package:e_commerce_app_firebase/view/home_screen/components/festured_button.dart';
 import 'package:e_commerce_app_firebase/widgets_common/home_buttons.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +10,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(12),
+      padding: EdgeInsets.all(8),
       color: lightGrey,
       width: context.screenWidth,
       height: context.screenHeight,
@@ -22,6 +23,7 @@ class HomeScreen extends StatelessWidget {
               color: lightGrey,
               child: TextFormField(
                 decoration: InputDecoration(
+                  border: InputBorder.none,
                   suffixIcon: Icon(Icons.search),
                   filled: true,
                   fillColor: whiteColor,
@@ -30,77 +32,235 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
             ),
-            VxSwiper.builder(
-                aspectRatio: 16 / 9,
-                autoPlay: true,
-                height: 150,
-                enlargeCenterPage: true,
-                itemCount: slidersList.length,
-                itemBuilder: (context, index) {
-                  return Image.asset(
-                    slidersList[index],
-                    fit: BoxFit.fill,
-                  )
-                      .box
-                      .rounded
-                      .clip(Clip.antiAlias)
-                      .margin(const EdgeInsets.symmetric(
-                          horizontal: 5, vertical: 15))
-                      .make();
-                }),
-            10.heightBox,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(
-                  2,
-                  (index) => homeButtons(
-                        height: context.screenHeight * 0.15,
-                        width: context.screenWidth / 2.5,
-                        icon: index == 0 ? icTodaysDeal : icFlashDeal,
-                        title: index == 0 ? todayDeal : flashSale,
-                      )),
-            ),
-            5.heightBox,
-            VxSwiper.builder(
-                aspectRatio: 16 / 9,
-                autoPlay: true,
-                height: 150,
-                enlargeCenterPage: true,
-                itemCount: secondSlidersList.length,
-                itemBuilder: (context, index) {
-                  return Image.asset(
-                    secondSlidersList[index],
-                    fit: BoxFit.fill,
-                  )
-                      .box
-                      .rounded
-                      .clip(Clip.antiAlias)
-                      .margin(const EdgeInsets.symmetric(
-                          horizontal: 5, vertical: 15))
-                      .make();
-                }),
-            10.heightBox,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(
-                  3,
-                  (index) => homeButtons(
-                        height: context.screenHeight * 0.15,
-                        width: context.screenWidth / 3.5,
-                        icon: index == 0
-                            ? icTopCategories
-                            : index == 1
-                                ? icBrands
-                                : icTopSeller,
-                        title: index == 0
-                            ? topCategories
-                            : index == 1
-                                ? brand
-                                : topSellers,
-                      )),
-            ),
+            // 5.heightBox,
+            Expanded(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  children: [
+                    VxSwiper.builder(
+                        aspectRatio: 16 / 9,
+                        autoPlay: true,
+                        height: 150,
+                        enlargeCenterPage: true,
+                        itemCount: slidersList.length,
+                        itemBuilder: (context, index) {
+                          return Image.asset(
+                            slidersList[index],
+                            fit: BoxFit.fill,
+                          )
+                              .box
+                              .rounded
+                              .clip(Clip.antiAlias)
+                              .margin(const EdgeInsets.symmetric(
+                                  horizontal: 5, vertical: 5))
+                              .make();
+                        }),
+                    5.heightBox,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: List.generate(
+                          2,
+                          (index) => homeButtons(
+                                height: context.screenHeight * 0.15,
+                                width: context.screenWidth / 2.5,
+                                icon: index == 0 ? icTodaysDeal : icFlashDeal,
+                                title: index == 0 ? todayDeal : flashSale,
+                              )),
+                    ),
+                    5.heightBox,
+                    VxSwiper.builder(
+                        aspectRatio: 16 / 9,
+                        autoPlay: false,
+                        height: 150,
+                        enlargeCenterPage: true,
+                        itemCount: secondSlidersList.length,
+                        itemBuilder: (context, index) {
+                          return Image.asset(
+                            secondSlidersList[index],
+                            fit: BoxFit.fill,
+                          )
+                              .box
+                              .rounded
+                              .clip(Clip.antiAlias)
+                              .margin(const EdgeInsets.symmetric(
+                                  horizontal: 5, vertical: 5))
+                              .make();
+                        }),
+                    5.heightBox,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: List.generate(
+                          3,
+                          (index) => homeButtons(
+                                height: context.screenHeight * 0.15,
+                                width: context.screenWidth / 3.5,
+                                icon: index == 0
+                                    ? icTopCategories
+                                    : index == 1
+                                        ? icBrands
+                                        : icTopSeller,
+                                title: index == 0
+                                    ? topCategories
+                                    : index == 1
+                                        ? brand
+                                        : topSellers,
+                              )),
+                    ),
+                    15.heightBox,
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: featuredCategories.text
+                          .color(darkFontGrey)
+                          .size(18)
+                          .fontFamily(semibold)
+                          .make(),
+                    ),
+                    15.heightBox,
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: List.generate(
+                            3,
+                            (index) => Column(
+                                  children: [
+                                    featuredButton(
+                                        icon: featuredImages1[index],
+                                        title: featuredTitles1[index]),
+                                    10.heightBox,
+                                    featuredButton(
+                                        icon: featuredImages2[index],
+                                        title: featuredTitles2[index]),
+                                  ],
+                                )).toList(),
+                      ),
+                    ),
+                    15.heightBox,
+                    Container(
+                      padding: const EdgeInsets.all(12),
+                      width: double.infinity,
+                      decoration: const BoxDecoration(
+                        color: redColor,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          featuredProduct.text.white
+                              .fontFamily(bold)
+                              .size(18)
+                              .make(),
+                          10.heightBox,
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              children: List.generate(
+                                  6,
+                                  (index) => Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Image.asset(
+                                            imgP1,
+                                            width: 130,
+                                            fit: BoxFit.cover,
+                                          ),
+                                          10.heightBox,
+                                          "Laptop 4GB/64GN"
+                                              .text
+                                              .fontFamily(semibold)
+                                              .color(darkFontGrey)
+                                              .make(),
+                                          10.heightBox,
+                                          "\$600"
+                                              .text
+                                              .color(redColor)
+                                              .fontFamily(bold)
+                                              .size(16)
+                                              .make()
+                                        ],
+                                      )
+                                          .box
+                                          .margin(EdgeInsets.symmetric(
+                                              horizontal: 4))
+                                          .white
+                                          .roundedSM
+                                          .padding(const EdgeInsets.all(8))
+                                          .make()),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
 
+                    15.heightBox,
+                    VxSwiper.builder(
+                        aspectRatio: 16 / 9,
+                        autoPlay: true,
+                        height: 150,
+                        enlargeCenterPage: true,
+                        itemCount: secondSlidersList.length,
+                        itemBuilder: (context, index) {
+                          return Image.asset(
+                            secondSlidersList[index],
+                            fit: BoxFit.fill,
+                          )
+                              .box
+                              .rounded
+                              .clip(Clip.antiAlias)
+                              .margin(const EdgeInsets.symmetric(
+                                  horizontal: 5, vertical: 5))
+                              .make();
+                        }),
 
+                    // All product section
+                    20.heightBox,
+                    GridView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: 6,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                mainAxisSpacing: 8,
+                                crossAxisSpacing: 8,
+                                mainAxisExtent: 260),
+                        itemBuilder: (context, index) {
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                imgP5,
+                                height: 185,
+                                width: 200,
+                                fit: BoxFit.cover,
+                              ),
+                              Spacer(),
+                              10.heightBox,
+                              "Laptop 4GB/64GN"
+                                  .text
+                                  .fontFamily(semibold)
+                                  .color(darkFontGrey)
+                                  .make(),
+                              10.heightBox,
+                              "\$600"
+                                  .text
+                                  .color(redColor)
+                                  .fontFamily(bold)
+                                  .size(16)
+                                  .make()
+                            ],
+                          )
+                              .box
+                              .margin(EdgeInsets.symmetric(horizontal: 4))
+                              .white
+                              .roundedSM
+                              .padding(const EdgeInsets.all(10))
+                              .make();
+                        })
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
